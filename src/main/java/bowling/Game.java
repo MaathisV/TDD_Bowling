609df194c3ffd512fcafb0e0ea75a289c7423910
Mathis <Mathis@Zenbook-M>
2025-06-22 11:45:42 +0200

    Gutter Game test

---
+++ src/main/java/bowling/Game.java
@@ -0,0 +1,13 @@
+package bowling;
+
+public class Game {
+
+	public void roll(int KnockedPins) {
+
+	}
+
+	public int score() {
+		return 0;
+	}
+
+}
---
+++ src/test/java/bowling/BowlingGameTest.java
@@ -0,0 +1,19 @@
+package bowling;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BowlingGameTest {
+
+	@Test
+	void testGutterGame() {
+		Game g = new Game();
+		for (int i = 0; i < 20; i++) {
+			g.roll(0);
+		}
+		int score = g.score();
+		assertEquals(0, score);
+	}
+
+}
